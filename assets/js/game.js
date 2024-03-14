06e67f15cdd2f4898e8cdd8e2c827a44ef5d02a3
AJRMRODMAR <antoniodejesusrodriguez928@gmail.com>
2024-03-13 23:13:08 -0600

    mujer, obstavculos

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,13 @@
 
 var time = new Date();
 var deltaTime = 0;
-
+function jugar(params) {
+    
 if(document.readyState === "complete" || document.readyState === "interactive"){
     setTimeout(Init, 1);
 }else{
     document.addEventListener("DOMContentLoaded", Init); 
+}
 }
 
 function Init() {
@@ -215,7 +217,7 @@ function GanarPuntos() {
 function GameOver() {
     Estrellarse();
     gameOver.style.display = "block";
-   Start();
+   document.getElementById("JugarB").innerHTML("reintentar")
 }
 
 function DetectarColision() {
